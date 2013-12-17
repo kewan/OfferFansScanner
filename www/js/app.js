c2265700bfe98762f30e738d5c8155db7c023450
Kewan Shunn <kewan@kewanshunn.com>
2013-12-17 21:42:44 +0000

    Add initial login and scan screens

--- www/js/app.js
+++ www/js/app.js
@@ -8,8 +8,10 @@ angular.module('myApp', [
     'myApp.memoryServices'
 ]).
 config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
+    $routeProvider.when('/scan', {templateUrl: 'partials/scan.html', controller: 'ScanCtrl'});
     $routeProvider.when('/employees', {templateUrl: 'partials/employee-list.html', controller: 'EmployeeListCtrl'});
     $routeProvider.when('/employees/:employeeId', {templateUrl: 'partials/employee-detail.html', controller: 'EmployeeDetailCtrl'});
     $routeProvider.when('/employees/:employeeId/reports', {templateUrl: 'partials/report-list.html', controller: 'ReportListCtrl'});
-    $routeProvider.otherwise({redirectTo: '/employees'});
+    $routeProvider.otherwise({redirectTo: '/login'});
 }]);
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,6 +12,38 @@ angular.module('myApp.controllers', [])
           $location.url(path);
         }
     }])
+    .controller('LoginCtrl', ['$scope', 'Employee', '$http', function ($scope, Employee, $http) {
+        // $scope.employees = Employee.query();
+        $scope.doLogin = function() {
+            $scope.error = {};
+
+            if ( $scope.user === undefined || !$scope.user.username || !$scope.user.password) {
+                $scope.error.message = 'Invalid login details';
+                return;
+            }
+
+            $http.post("http://offerfans.ngrok.com/api/v1/auth/login", $scope.user)
+                 .success(function(data) {
+                    window.localStorage.setItem("access_token", data.token);
+                    window.localStorage.setItem("username", $scope.user.username);
+                    window.location = "/#/scan";
+                    return true;
+                 })
+                 .error(function(data, status, headers, config) {
+                    $scope.error.message = data.error;
+                    return;
+                 });
+        }
+    }])
+    .controller('ScanCtrl', ['$scope', 'Employee', function ($scope, Employee) {
+
+        if (!window.localStorage.getItem("access_token")) {
+            window.location = "/#/login";
+            return false;
+        }
+
+        $scope.user  = { username: window.localStorage.getItem("username") };
+    }])
     .controller('EmployeeListCtrl', ['$scope', 'Employee', function ($scope, Employee) {
         $scope.employees = Employee.query();
     }])
