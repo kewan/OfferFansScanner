2110adfe0fc1ddca5dcaa799280f903915dea202
Kewan Shunn <kewan@kewanshunn.com>
2013-12-18 11:17:12 +0000

    Adding a scan function to app

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -47,6 +47,33 @@ angular.module('myApp.controllers', [])
         }
 
         $scope.user  = { username: window.localStorage.getItem("username") };
+
+        $scope.scan = function() {
+            var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+            scanner.scan( function (result) { 
+
+                alert("We got a barcode\n" + 
+                "Result: " + result.text + "\n" + 
+                "Format: " + result.format + "\n" + 
+                "Cancelled: " + result.cancelled);  
+
+               console.log("Scanner result: \n" +
+                    "text: " + result.text + "\n" +
+                    "format: " + result.format + "\n" +
+                    "cancelled: " + result.cancelled + "\n");
+                document.getElementById("info").innerHTML = result.text;
+                console.log(result);
+                /*
+                if (args.format == "QR_CODE") {
+                    window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+                }
+                */
+
+            }, function (error) { 
+                console.log("Scanning failed: ", error); 
+            } );
+        }
     }])
     .controller('EmployeeListCtrl', ['$scope', 'Employee', function ($scope, Employee) {
         $scope.employees = Employee.query();
