effc92c196cbfdd1ba7ab0950d0a1a96c814c9d5
Kewan Shunn <kewan@kewanshunn.com>
2013-12-18 22:30:16 +0000

    Move scanner logic in to scanner service

--- www/js/scanner-service.js
+++ www/js/scanner-service.js
@@ -3,15 +3,36 @@
 angular.module('myApp.scannerService', ['fsCordova'])
     .factory('Scanner', ['CordovaService',
         function (CordovaService) {
-
-            CordovaService.ready.then(function() {
-              console.log("cordova is now ready sir");
-            });
-
-
             return {
               scan: function() {
-                console.log("Trying to scan");
+                CordovaService.ready.then(function() {
+                  console.log("cordova is now ready sir");
+                  var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+                  scanner.scan( function (result) { 
+
+                      alert("We got a barcode\n" + 
+                      "Result: " + result.text + "\n" + 
+                      "Format: " + result.format + "\n" + 
+                      "Cancelled: " + result.cancelled);  
+
+                     console.log("Scanner result: \n" +
+                          "text: " + result.text + "\n" +
+                          "format: " + result.format + "\n" +
+                          "cancelled: " + result.cancelled + "\n");
+                      document.getElementById("info").innerHTML = result.text;
+                      console.log(result);
+                      /*
+                      if (args.format == "QR_CODE") {
+                          window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+                      }
+                      */
+
+                  }, function (error) { 
+                      console.log("Scanning failed: ", error); 
+                  } );
+                  
+                });
               }
             }
 
