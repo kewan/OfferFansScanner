0b906d7fa1bc45504aadaa0f28861efd93db37be
Kewan Shunn <kewan@kewanshunn.com>
2013-12-19 00:44:54 +0000

    Tidyup scan service

--- www/js/scanner-service.js
+++ www/js/scanner-service.js
@@ -1,75 +1,41 @@
 'use strict';
 
-function KewScan(CardovaService) {
-
-  CordovaService.ready.then(function() {
-    this.scanner = cordova.require("cordova/plugin/BarcodeScanner");
-
-    this.scan = function() {
-      return this.scanner.scan(function(result) {
-        if (result.cancelled) {
-          return {};
-        }
-
-        if (!result.text) {
-          return { error: 'Can not read code' };
-        }
-
-        if(result.format != "QR_CODE") {
-          return { error: "Unknown barcode format" };
-        }
-
-        return { code: result.text };
-
-      }, function(error) {
-        return { error: "Scanning failed: " + error};
-      })
-    }
-
-  });
-
-}
-
-/*
-return {
-  scan: function() {
-
-    CordovaService.ready.then(function() {
-
-      var scanner = cordova.require("cordova/plugin/BarcodeScanner");
-
-      scanner.scan( function (result) { 
-
-         console.log("Scanner result: \n" +
-              "text: " + result.text + "\n" +
-              "format: " + result.format + "\n" +
-              "cancelled: " + result.cancelled + "\n");
-
-          // if result.cancelled {
-          //     return;
-          // }
-
-          // if (!result.text) {
-          //     response.error = "Can not read barcode";
-          //     return;
-          // }
-
-          // if (args.format != "QR_CODE") {
-          //     response.error = "Unknown barcode format";
-          //     return;
-          // }                      
-
-      }, function (error) { 
-          console.log("Scanning failed: ", error);
-      } );
-
-    });
-  }
-}
-*/
-
 angular.module('myApp.scannerService', ['fsCordova'])
     .factory('Scanner', ['CordovaService',
         function (CordovaService) {
-          return new KewScan(CardovaService);
+          return {
+            scan: function() {
+
+              return CordovaService.ready.then(function() {
+
+                var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+                scanner.scan( function (result) { 
+
+                   console.log("Scanner result: \n" +
+                        "text: " + result.text + "\n" +
+                        "format: " + result.format + "\n" +
+                        "cancelled: " + result.cancelled + "\n");
+
+                    if (result.cancelled) {
+                        return {};
+                    }
+
+                    if (!result.text) {
+                        return { error: "Can not read barcode"};
+                    }
+
+                    if (result.format != "QR_CODE") {
+                        return { error: "Unknown barcode format" };
+                    }
+
+                    return { code: result.text };
+
+                }, function (error) { 
+                  return { error: "Scanning failed: " + error };
+                } );
+
+              });
+            }
+          }
         }]);
