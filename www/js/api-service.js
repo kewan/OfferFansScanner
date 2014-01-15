e55a152763cf05190b16e492558296eddeb9ce7c
Kewan Shunn <kewan@kewanshunn.com>
2014-01-15 16:34:01 +0000

    Add different api urls for staging and production

--- www/js/api-service.js
+++ www/js/api-service.js
@@ -4,8 +4,29 @@ angular.module('myApp.apiService', [])
     .factory('Api', ['$http',
         function ($http) {
           return {
+
+            env: 'development',
+
+            options: {
+              development: { url: "http://offerfans.ngrok.com", version: 'v1' },
+              staging:     { url: "https://stg.offerfans.com", version: 'v1' },
+              production:  { url: "https://www.offerfans.com", version: 'v1' }
+            },
+
+            build_url: function(path) {
+
+              if (!this.options[ this.env ]) {
+                this.env = 'production';
+              }
+
+              var url = this.options[this.env]['url'] + '/api/' + this.options[this.env]['version'] + path;
+
+              console.log("API url: "+url);
+              return url;
+            },
+
             login: function(user, onSuccess, onFailure) {
-              $http.post("http://offerfans.ngrok.com/api/v1/auth/login", user)
+              $http.post(this.build_url("/auth/login"), user)
                    .success(onSuccess)
                    .error(onFailure);
             },
@@ -18,7 +39,7 @@ angular.module('myApp.apiService', [])
               }
 
               // put access_token in header
-              $http.put("http://offerfans.ngrok.com/api/v1/redeem/"+code, {}, options)
+              $http.put(this.build_url("/redeem/"+code), {}, options)
                    .success(onSuccess)
                    .error(onFailure);
             }
